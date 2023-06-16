fc96b3460af8af959926d4bdc56498dda45b124c
Ruslan Yeremin <prmoriarty212@gmail.com>
2023-06-16 16:15:03 +0300

    init commit

---
+++ types/index.ts
@@ -0,0 +1,16 @@
+import { manufacturers } from './../constants/index';
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    title: string;
+    containerStyles?:string;
+    handleClick?:
+    MouseEventHandler<HTMLButtonElement>;
+    btnType?: 'button' | 'submit'
+
+}
+
+export interface SearchManufacturerProps {
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
+}
